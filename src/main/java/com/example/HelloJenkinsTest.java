4d175b66bf4966c1bcb187f46eb3372e7377e49e
nikitavpatil74 <nikitavpatil74@gmail.com>
2025-05-30 10:31:23 +0000

    hello-jenkins project created

---
+++ src/main/java/com/example/HelloJenkinsTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloJenkinsTest {
+
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
+
